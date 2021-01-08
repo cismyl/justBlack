4c533791883968c6d14f28ec8973ee67b508c680
coding-ground <66957781+coding-ground@users.noreply.github.com>
2021-01-08 13:27:16 +0900

    upload app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import tkinter
+import pyautogui
+import time
+
+root = tkinter.Tk()
+root.title("JUST BLACK.")
+root.geometry("100x100+0+0")
+root.attributes("-fullscreen", True)
+root.resizable(True, True)
+root.config(bg="#000")
+
+root.mainloop()
